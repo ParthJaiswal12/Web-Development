a4d744f1a55fdf6431fddd60a20f2be090ad5406
Parth Jaiswal <parth.jaiswal2905@gmail.com>
2026-02-15 20:01:51 +0530

    2nd JS added

---
+++ JS/2_Variable_DataType/script.js
@@ -0,0 +1,42 @@
+console.log("This is Console.log to print on console");
+
+let MyName = "Parth"; //variable declaration
+
+console.log(MyName)
+
+let a = 1;
+console.log("here a is " + a);
+a = 2;
+console.log("here a is " + a);
+
+let b = 2;
+console.log("here b is " + b);
+b = "String";
+console.log("here b is " + b);
+
+const c = 12; // value of const can't be change
+
+//Primitive Data Type
+// 1. String - "Parth"
+// 2. Int - 12
+// 3. bigint - 23425654231345321
+// 4. Boolen - True/False
+// 5. Null - Null
+// 6. Undefined - undefined
+// 7. Symbol - Symbol("if")
+
+// Reference Data Type
+// 1. array
+// 2. function
+// 3. object
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof null);
+
+
+
+
+
+
